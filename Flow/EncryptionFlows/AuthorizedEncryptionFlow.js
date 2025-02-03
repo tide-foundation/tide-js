0411f618782ed877af8ca830fe06ad40cb51777c
sundayScoop <killer.of.time@protonmail.com>
2025-02-03 18:51:38 +1000

    moved decryption flow to encryption flow to keep in same object

--- Flow/EncryptionFlows/AuthorizedEncryptionFlow.js
+++ Flow/EncryptionFlows/AuthorizedEncryptionFlow.js
@@ -1,6 +1,6 @@
 import { ElGamal, Serialization } from "../../Cryptide/index.js";
 import { Ed25519PublicComponent } from "../../Cryptide/Components/Schemes/Ed25519/Ed25519Components.js";
-import { encryptData, encryptDataRawOutput } from "../../Cryptide/Encryption/AES.js";
+import { decryptDataRawOutput, encryptData, encryptDataRawOutput } from "../../Cryptide/Encryption/AES.js";
 import { base64ToBytes, base64UrlToBase64, numberToUint8Array, StringFromUint8Array, StringToUint8Array } from "../../Cryptide/Serialization.js";
 import { CurrentTime } from "../../Tools/Utils.js";
 import BaseTideRequest from "../../Models/BaseTideRequest.js";
@@ -8,6 +8,7 @@ import NetworkClient from "../../Clients/NetworkClient.js";
 import dVVKSigningFlow from "../SigningFlows/dVVKSigningFlow.js";
 import { GenSessKey, GetPublic } from "../../Cryptide/Math.js";
 import SerializedField from "../../Models/SerializedField.js";
+import dVVKDecryptionFlow from "../DecryptionFlows/dVVKDecryptionFlow.js";
 /**
  * 
  * @param {{
@@ -127,4 +128,89 @@ export function AuthorizedEncryptionFlow(config){
                 sig)
         )
     }
+
+    /**
+         * 
+         * @param {[
+         * {
+         *    encrypted: Uint8Array,
+         *    tags: string[]
+         * }
+         * ]} datasToDecrypt 
+         */
+        encryptionFlow.decrypt = async function(datasToDecrypt){
+            // Deserialize all datasToDecrypt + include tags in object
+            const deserializedDatas = datasToDecrypt.map(d => {
+                const b = SerializedField.deserialize(d.encrypted);
+                if(b.signature == null) throw Error("Signature must be provided in Tide Serialized Data to an Authorized Decryption");
+                const tags_b = d.tags.map(t => StringToUint8Array(t));
+                return {
+                    ...b,
+                    tags: tags_b
+                }
+            })
+    
+            // Get orks to apply vvk
+            const pre_info = getVVKInfo();
+    
+            const entries = deserializedDatas.map((data, i) => {
+                if(data.encKey){
+                    // We must decrypt the encrypted key, not the data itself
+                    const entry = Serialization.CreateTideMemory(data.encKey, 4 + data.encKey.length + 4 + data.signature.length + 4 + data.timestamp.length + data.tags.reduce((sum, next) => sum + 4 + next.length, 0));
+                    Serialization.WriteValue(entry, 1, data.signature); // won't be null
+                    Serialization.WriteValue(entry, 2, data.timestamp);
+                    data.tags.forEach((tag, j) => {
+                        Serialization.WriteValue(entry, j+3, tag); // + 3 as we start at index 3
+                    })
+                    return entry;
+                }else{
+                    // decrypt data directly
+                    const entry = Serialization.CreateTideMemory(data.encFieldChk, 4 + data.encFieldChk.length + 4 + data.signature.length + 4 + data.timestamp.length + data.tags.reduce((sum, next) => sum + 4 + next.length, 0));
+                    Serialization.WriteValue(entry, 1, data.signature); // won't be null
+                    Serialization.WriteValue(entry, 2, data.timestamp);
+                    data.tags.forEach((tag, j) => {
+                        Serialization.WriteValue(entry, j+3, tag); // + 3 as we start at index 3
+                    })
+                    return entry;
+                }
+                
+            })
+    
+            const draft = Serialization.CreateTideMemory(entries[0], entries.reduce((sum, next) => sum +  4 + next.length, 0));
+            for(let i = 1; i < entries.length; i++){
+                Serialization.WriteValue(draft, i, entries[i]);
+            }
+    
+            const decryptionRequest = new BaseTideRequest("SelfDecrypt", "1", "AccessToken:1", draft);
+    
+            // Deserialize token to retrieve vuid - if it exists
+            const vuid = JSON.parse(StringFromUint8Array(base64ToBytes(base64UrlToBase64(this.token.split(".")[1])))).vuid; // get vuid field from jwt payload in 1 line
+            if(vuid) decryptionRequest.dyanmicData = StringToUint8Array(vuid);
+            
+            // Set the Authorization token as the authorizer for the request
+            decryptionRequest.addAuthorizer(StringToUint8Array(this.token));
+            decryptionRequest.addAuthorizerCertificate(new Uint8Array());// special case where other field isn't required
+            decryptionRequest.addAuthorization(new Uint8Array()); // special case where other field isn't required
+    
+            await pre_info;
+    
+            const flow = new dVVKDecryptionFlow(this.vvkId, this.vvkInfo.UserPublic, this.vvkInfo.OrkInfo, this.sessKey, this.gSessKey, this.voucherURL);
+            const dataKeys = await flow.start(decryptionRequest);
+    
+            // Decrypt all datas
+            const decryptedDatas = await Promise.all(deserializedDatas.map(async (data, i) => {
+                // if encKey exists - decrypt with elgamal that
+                // then decrypt encField with key
+                if(data.encKey){
+                    const key = await decryptDataRawOutput(data.encKey.slice(32), dataKeys[i]);
+                    return await decryptDataRawOutput(data.encFieldChk, key);
+                }else{
+                    // else - decrypt encField with elgamal
+                    return await decryptDataRawOutput(data.encFieldChk.slice(32), dataKeys[i]);
+                }
+            }));
+    
+            // Return as bytes
+            return decryptedDatas;
+        }
 }
