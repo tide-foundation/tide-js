f6916958952935db1b61fe4d03620457093b2d3f
sundayScoop <killer.of.time@protonmail.com>
2025-11-19 15:24:21 +1000

    add policy

--- Models/ModelRegistry.js
+++ Models/ModelRegistry.js
@@ -6,40 +6,40 @@ import BaseTideRequest from "./BaseTideRequest.js";
 import Policy from "./Policy.js";
 import { Serialization } from "../Cryptide/index.js";
 
-export class ModelRegistry{
+export class ModelRegistry {
     /**
      * @returns {HumanReadableModelBuilder}
      */
-    static getHumanReadableModelBuilder(reqId, data){
+    static getHumanReadableModelBuilder(reqId, data) {
         const r = BaseTideRequest.decode(data);
         const c = modelBuildersMap[r.id()];
-        if(!c) throw Error("Could not find model: " + r.id());
+        if (!c) throw Error("Could not find model: " + r.id());
         return c.create(data, reqId);
     }
 }
 
-export class HumanReadableModelBuilder{
+export class HumanReadableModelBuilder {
     _humanReadableName = null;
-    constructor(data, reqId){
-        if(data){
+    constructor(data, reqId) {
+        if (data) {
             this._data = data;
             this._draft = GetValue(this._data, 3);
             this.request = BaseTideRequest.decode(data);
         }
         this.reqId = reqId;
     }
-    static create(data, reqId){
+    static create(data, reqId) {
         return new this(data, reqId);
     }
-    getApprovalRecieved(){
+    getApprovalRecieved() {
         // how many approvals have been already submitted for this model
         const authorizers = GetValue(this._data, 6);
         let i = 0;
         let res = {};
-        while(TryGetValue(authorizers, i, res)){i++;}
+        while (TryGetValue(authorizers, i, res)) { i++; }
         return i;
     }
-    getApprovalsRequired(){
+    getApprovalsRequired() {
         const policy = new Policy(GetValue(this._data, 9));
 
 
@@ -50,38 +50,38 @@ export class HumanReadableModelBuilder{
 
         return policy.params.getParameter("threshold", Number);
     }
-    getDetailsMap(){
+    getDetailsMap() {
         // the summary
         return [];
     }
-    getRequestDataJson(){
+    getRequestDataJson() {
         // raw json
         return {};
     }
 
-    getExpiry(){
+    getExpiry() {
         return this.request.expiry;
     }
 
-    async getDataToApprove(){
+    async getDataToApprove() {
         return this.request.dataToApprove();
     }
 }
 
 // MODELS ----------------------------------------------------------------
-class UserContextSignRequestBuilder extends HumanReadableModelBuilder{
+class UserContextSignRequestBuilder extends HumanReadableModelBuilder {
     _name = "UserContext"; // Model ID
     _humanReadableName = "User Access Change";
     _version = "1";
     get _id() { return this._name + ":" + this._version; }
 
-    constructor(data, reqId){
+    constructor(data, reqId) {
         super(data, reqId);
     }
-    static create(data, reqId){
+    static create(data, reqId) {
         return super.create(data, reqId);
     }
-    getRequestDataJson(){
+    getRequestDataJson() {
         // deserialize draft here and return a pretty object for user
         let prettyObject = {};
 
@@ -89,15 +89,15 @@ class UserContextSignRequestBuilder extends HumanReadableModelBuilder{
         // make sure user context is JSON
         let cont = true;
         prettyObject.UserContexts = [];
-        while(cont){
-            try{prettyObject.UserContexts.push(JSON.parse(StringFromUint8Array(GetValue(this._draft, draftIndex))));draftIndex++;}
-            catch{cont = false;}
+        while (cont) {
+            try { prettyObject.UserContexts.push(JSON.parse(StringFromUint8Array(GetValue(this._draft, draftIndex)))); draftIndex++; }
+            catch { cont = false; }
         }
-        
+
         // return a nice object of InitCert? and usercontexts
         return prettyObject;
     }
-    getDetailsMap(){
+    getDetailsMap() {
         // deserialize draft here and return a pretty object for user
         let prettyObject = {};
 
@@ -105,9 +105,9 @@ class UserContextSignRequestBuilder extends HumanReadableModelBuilder{
         // make sure user context is JSON
         let cont = true;
         prettyObject.UserContexts = [];
-        while(cont){
-            try{prettyObject.UserContexts.push(JSON.parse(StringFromUint8Array(GetValue(this._draft, draftIndex))));draftIndex++;}
-            catch{cont = false;}
+        while (cont) {
+            try { prettyObject.UserContexts.push(JSON.parse(StringFromUint8Array(GetValue(this._draft, draftIndex)))); draftIndex++; }
+            catch { cont = false; }
         }
 
         // Create summary
@@ -116,8 +116,8 @@ class UserContextSignRequestBuilder extends HumanReadableModelBuilder{
         // All clients will be either realm-management or under resource_management
         let clients = [];
         prettyObject.UserContexts.map(c => {
-            if(c.realm_access) clients.push("realm_access");
-            if(typeof c.resource_access === "object"){
+            if (c.realm_access) clients.push("realm_access");
+            if (typeof c.resource_access === "object") {
                 clients.push(...Object.keys(c.resource_access));
             }
         })
@@ -129,26 +129,26 @@ class UserContextSignRequestBuilder extends HumanReadableModelBuilder{
     }
 }
 
-export class OffboardSignRequestBuilder extends HumanReadableModelBuilder{
+export class OffboardSignRequestBuilder extends HumanReadableModelBuilder {
     _name = "Offboard";
     _version = "1";
     _humanReadableName = "Cancel Tide Subscription and Protection";
 
     get _id() { return this._name + ":" + this._version; }
-    constructor(data, reqId){
+    constructor(data, reqId) {
         super(data, reqId);
     }
-    getDetailsMap(){
+    getDetailsMap() {
         let summary = {};
         summary["WARNING WARNING WARNING"] = "";
         summary["APPROVING THIS REQUEST WILL CRIPPLE YOUR LICENSED TIDE ACCOUNT"] = "";
         summary["ONLY APPROVE THIS REQUEST IF YOU INTEND TO OFFBOARD FROM THE TIDE NETWORK"] = "";
-        summary["THIS ACTION IS UNRECOVERABLE"] =  "";
+        summary["THIS ACTION IS UNRECOVERABLE"] = "";
 
-        
+
         return summary;
     }
-    getRequestDataJson(){
+    getRequestDataJson() {
         const vrk = Bytes2Hex(GetValue(this._draft, 0));
         let body = {
             "Vendor Rotating Key for Offboarding": vrk
@@ -157,37 +157,69 @@ export class OffboardSignRequestBuilder extends HumanReadableModelBuilder{
     }
 }
 
-class LicenseSignRequestBuilder extends HumanReadableModelBuilder{
+class PolicySignRequestBuilder extends HumanReadableModelBuilder {
+    _name = "Policy";
+    _version = "1";
+    _humanReadableName = "Approve new policy for use with Tide";
+    get _id() { return this._name + ":" + this._version; }
+    constructor(data, expiry) {
+        super(data, expiry);
+    }
+    getDetailsMap() {
+        let summary = {};
+        const policy = new Policy(GetValue(this._draft, 0));
+        policy.params.entries().forEach(([key, value]) => {
+            if (!(value instanceof Uint8Array)) summary[key] = value;
+        });
+        let res = { value: null };
+        if (TryGetValue(this._draft, 1, res)) {
+            // contract is also included
+            const contractType = StringFromUint8Array(GetValue(res.value, 0));
+            summary["Contract To Upload Type"] = contractType;
+        }
+        return summary;
+    }
+    getRequestDataJson() {
+        let body = {};
+        let res = { value: null };
+        if (TryGetValue(this._draft, 1, res)) {
+            body["Contract Data To Upload Base64"] = bytesToBase64(GetValue(res.value, 1));
+        }
+        return body;
+    }
+}
+
+class LicenseSignRequestBuilder extends HumanReadableModelBuilder {
     _name = "RotateVRK";
     _version = "1";
     _humanReadableName = "Renew License with New Permissions";
 
     get _id() { return this._name + ":" + this._version; }
-    constructor(data, expiry){
+    constructor(data, expiry) {
         super(data, expiry);
     }
-    getDetailsMap(){
+    getDetailsMap() {
         const authPack = new AuthorizerPack(this._draft);
 
         let summary = [];
         summary["Signing new license"] = authPack.Authorizer.GVRK.Serialize().ToString();
 
         summary["Approved Models to Sign"] = authPack.SignModels;
-        
+
         return summary;
     }
 }
-class TestInitSignRequestBuilder extends HumanReadableModelBuilder{
+class TestInitSignRequestBuilder extends HumanReadableModelBuilder {
     _name = "TestInit";
     _version = "1";
     _humanReadableName = "Test Tide Request";
     get _id() { return this._name + ":" + this._version; }
-    constructor(data, expiry){
+    constructor(data, expiry) {
         super(data, expiry);
     }
-    getDetailsMap(){
+    getDetailsMap() {
         let summary = [];
-        summary["Draft Detail"] = StringFromUint8Array(this._draft);        
+        summary["Draft Detail"] = StringFromUint8Array(this._draft);
         return summary;
     }
 }
@@ -196,5 +228,6 @@ const modelBuildersMap = {
     [new UserContextSignRequestBuilder()._id]: UserContextSignRequestBuilder,
     [new OffboardSignRequestBuilder()._id]: OffboardSignRequestBuilder,
     [new LicenseSignRequestBuilder()._id]: LicenseSignRequestBuilder,
-    [new TestInitSignRequestBuilder()._id]: TestInitSignRequestBuilder
+    [new TestInitSignRequestBuilder()._id]: TestInitSignRequestBuilder,
+    [new PolicySignRequestBuilder()._id]: PolicySignRequestBuilder
 }
